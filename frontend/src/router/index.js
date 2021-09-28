46088a0016edaa6e17db4b2827f011e77a1e7f24
Olivein <gokcedalkiran10@gmail.com>
2021-09-28 22:08:08 +0200

    Edit frontend index-router file

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/home.vue'
+import CustomerList from '../views/customer-list.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'CustomerList',
+    component: CustomerList,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/customers/:id',
+    name: 'CustomerDetail',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/about.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../views/customer-detail.vue'),
   },
 ]
 
