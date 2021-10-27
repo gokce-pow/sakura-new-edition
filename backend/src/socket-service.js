937d55e6b676d1d163d400d54df29842e3b7efb2
Olivein <gokcedalkiran10@gmail.com>
2021-10-27 21:14:38 +0200

    Edit socket.service file

--- backend/src/socket-service.js
+++ backend/src/socket-service.js
@@ -1,20 +1,35 @@
 const io = require('socket.io')()
 
 io.on('connect', socket => {
-  socket.emit('hello world!')
+  socket.emit('connection established')
 
-  setInterval(() => {
-    socket.emit('hello world!')
-  }, 2000)
+  //   setInterval(() => {
+  //     socket.emit('hello world!')
+  //   }, 2000)
 
-  socket.on('new message', (number, cb) => {
-    console.log('a new message received with a number', number)
-    console.log('replying with', number + 1)
-    cb(number)
+  //   socket.on('new message', (number, cb) => {
+  //     console.log('a new message received with a number', number)
+  //     console.log('replying with', number + 1)
+  //     cb(number)
+  //   })
+
+  //   socket.on('another api', cb => {
+  //     cb('another api response')
+  //   })
+  socket.on('new message', (streamId, message) => {
+    socket.to(streamId).emit('new live mesage', message)
   })
 
-  socket.on('another api', cb => {
-    cb('another api response')
+  socket.on('join stream', streamId => {
+    socket.join(streamId)
+  })
+
+  socket.on('go live', (customerId, cb) => {
+    console.log(`${customerId} is going live`)
+
+    socket.broadcast.emit('new live stream', customerId)
+    socket.join(customerId)
+    cb(true)
   })
 })
 
