270fb395dbd7c0d0ca417cf27216f1f1899718bf
Olivein <gokcedalkiran10@gmail.com>
2021-10-27 23:11:00 +0200

    Add style/ template to home and customerCard.vue

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -5,6 +5,18 @@ export default {
 </script>
 
 <template lang="pug">
-    h1 Sakura Home
-      h2 Welcome
+  .box
+    h1 Welcome to Sakura
+    h2 Easiest way to get the service you want
+    h3 Get quotes for free, top quality 100.000 professionals are ready for you.
 </template>
+
+<style lang="scss" scoped>
+.box {
+  padding: 0.2rem;
+  border: 10px solid rgba(84, 114, 114, 0.836);
+  background: rgb(130, 230, 216);
+  border-radius: 0.3rem;
+  box-shadow: blanchedalmond;
+}
+</style>
