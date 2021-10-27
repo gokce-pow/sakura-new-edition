981e6d98320d9f5e5a81fa34a145317d34a62ffc
Olivein <gokcedalkiran10@gmail.com>
2021-10-27 21:23:14 +0200

    Add goLive function,socket.on method,mutations

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -11,54 +11,54 @@ Vue.use(Vuex)
 
 const socket = io()
 
-socket.on('hello world!', () => {
-  console.log('we received message from the websocket server!')
-})
+// socket.on('hello world!', () => {
+//   console.log('we received message from the websocket server!')
+// })
 
-setInterval(() => {
-  const number = Math.random()
-  console.log(`i'm sending a request`, number)
-  socket.emit('new message', number, res => {
-    console.log('this is a response', res)
-  })
+// setInterval(() => {
+//   const number = Math.random()
+//   console.log(`i'm sending a request`, number)
+//   socket.emit('new message', number, res => {
+//     console.log('this is a response', res)
+//   })
 
-  socket.emit('another api', res => {
-    console.log(res)
-  })
-}, 3000)
+//   socket.emit('another api', res => {
+//     console.log(res)
+//   })
+// }, 3000)
 
 const mutations = {
   INCREMENT_COUNT: 'increment count',
   SET_USER: 'set user',
-  // SET_LIVE_STREAM: 'set live stream',
-  // ADD_LIVE_STREAM: 'add live stream',
-  // ADD_MESSAGE_TO_LIVE_STREAM: 'add message to live stream',
+  SET_LIVE_STREAM: 'set live stream',
+  ADD_LIVE_STREAM: 'add live stream',
+  ADD_MESSAGE_TO_LIVE_STREAM: 'add message to live stream',
 }
 
 const store = new Vuex.Store({
   state: {
     count: 0,
-    user: null,
-    // currentLiveStream: null,
-    // liveStreams: [],
-    // liveStreamMessages: [],
+    customer: null,
+    currentLiveStream: null,
+    liveStreams: [],
+    liveStreamMessages: [],
   },
   mutations: {
     [mutations.INCREMENT_COUNT](state) {
       state.count++
     },
-    [mutations.SET_USER](state, user) {
-      state.user = user
+    [mutations.SET_USER](state, customer) {
+      state.customer = customer
     },
-    // [mutations.SET_LIVE_STREAM](state, live) {
-    //   state.currentLiveStream = live
-    // },
-    // [mutations.ADD_LIVE_STREAM](state, stream) {
-    //   state.liveStreams.push(stream)
-    // },
-    // [mutations.ADD_MESSAGE_TO_LIVE_STREAM](state, message) {
-    //   state.liveStreamMessages.push(message)
-    // },
+    [mutations.SET_LIVE_STREAM](state, live) {
+      state.currentLiveStream = live
+    },
+    [mutations.ADD_LIVE_STREAM](state, stream) {
+      state.liveStreams.push(stream)
+    },
+    [mutations.ADD_MESSAGE_TO_LIVE_STREAM](state, message) {
+      state.liveStreamMessages.push(message)
+    },
   },
   actions: {
     incrementCount({ commit }) {
@@ -87,7 +87,8 @@ const store = new Vuex.Store({
     async login({ commit }, credentials) {
       try {
         const user = await axios.post('/api/account/session', credentials)
-        commit(mutations.SET_USER, user.data)
+        console.log('User: ', user)
+        commit(mutations.SET_USER, user.data || null)
       } catch (e) {
         throw e
       }
@@ -99,18 +100,19 @@ const store = new Vuex.Store({
       await axios.delete('/api/account/session')
       commit(mutations.SET_USER, null)
     },
-    /*    async goLive({ state, commit }) {
-      socket.emit('go live', state.user._id, status => {
-        commit(mutations.SET_LIVE_STREAM, state.user._id)
+    async goLive({ state, commit }) {
+      socket.emit('go live', state.customer._id, status => {
+        commit(mutations.SET_LIVE_STREAM, state.customer._id)
       })
     },
+
     async addLiveStream({ state, commit }, stream) {
       commit(mutations.ADD_LIVE_STREAM, stream)
     },
     async sendMessageToLiveStream({ state, commit }, body) {
       const message = {
         body,
-        author: state.user.name,
+        author: state.customer.name,
       }
       commit(mutations.ADD_MESSAGE_TO_LIVE_STREAM, message)
       socket.emit('new message', state.currentLiveStream, message)
@@ -118,9 +120,17 @@ const store = new Vuex.Store({
     async joinStream({ state, commit }, stream) {
       socket.emit('join stream', stream)
       commit(mutations.SET_LIVE_STREAM, stream)
-    }, */
+    },
   },
   modules: {},
+})
+
+socket.on('new live stream', customer => {
+  store.dispatch('addLiveStream', customer)
+})
+
+socket.on('new live stream message', message => {
+  store.commit(mutations.ADD_MESSAGE_TO_LIVE_STREAM, message)
 })
 
 export default async function init() {
