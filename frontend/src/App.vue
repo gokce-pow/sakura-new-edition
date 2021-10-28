cdb289523dc25ce6a8f978628cae3e9652c4f676
Olivein <gokcedalkiran10@gmail.com>
2021-10-28 16:37:22 +0200

    Change font for App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -29,7 +29,7 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
